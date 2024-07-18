7c0a1db0d236164272bd53fe6d82ec8a995de238
phoen1xrise <daniebester1988@gmail.com>
2024-07-18 15:36:32 +0200

    Add files via upload

--- sha256_tool.py
+++ sha256_tool.py
@@ -1,24 +1,24 @@
 import hashlib
 
-def sha256_hash_gen_file(user_file):
+def sha256_hash_file(user_file):
     with open(user_file, 'rb') as f:
-        return hashlib.file_digest(f, "sha256").hexdigest()
-
-def sha256_hash_gen_str(user_str):
-    return hashlib.sha256(bytes(user_str, encoding="utf-8")).hexdigest()
+        return hashlib.file_digest(f, 'sha256').hexdigest()
+    
+def sha256_hash_str(user_str):
+    return hashlib.sha256(user_str.encode()).hexdigest()
 
 def prompt_user():
-    print("Do you want to SHA-256 hash a file or string?")
+    print('Do you want to hash a file or a string? ')
     response = ''
-    while response not in {"file", "string"}:
-        response = input("Please enter file or string: ").lower()
+    while response not in {'file', 'string'}:
+        response = input("Enter 'file' or 'string': ")
     return response
 
 user_response = prompt_user()
 
-if user_response == "string":
-    user_str = input("Enter the string: ")
-    print(sha256_hash_gen_str(user_str))
-elif user_response == "file":
-    user_file = input("Enter the file name: ")
-    print(sha256_hash_gen_file(user_file))
+if user_response == 'file':
+    user_file = input('Enter the file name: ')
+    print(sha256_hash_file(user_file))
+elif user_response == 'string':
+    user_str = input('Enter the string: ')
+    print(sha256_hash_str(user_str))
